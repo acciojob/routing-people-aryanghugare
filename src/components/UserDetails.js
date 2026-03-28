3010cdf19ccfa4f90e2e26605c485b01a9737651
Aryan <aryanghugare75@gmail.com>
2026-03-28 13:13:57 +0530

    Done

--- src/components/UserDetails.js
+++ src/components/UserDetails.js
@@ -1,42 +1,35 @@
-import React from 'react';
-import { useParams } from 'react-router-dom';
-import { useState,useEffect } from 'react';
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
 
 function UserDetails() {
+    const { id } = useParams();
+    const [user, setUser] = useState(null);
 
-const [users,setUsers] = useState({});
-const [loading,setLoading] = useState(true);
+    useEffect(() => {
+        async function fetchUser() {
+            const response = await fetch(`https://jsonplaceholder.typicode.com/users/${id}`);
+            const users = await response.json();
+            setUser(users);
+        }
+        setTimeout(() => {
+            fetchUser();
+        }, 1000);
+    }, [id]);
 
-const {id}= useParams();
+    if (!user) {
+        return <div>Loading...</div>
+    }
 
-useEffect(() => {
-setLoading(true);
-fetch(`https://jsonplaceholder.typicode.com/users/${id}`)
-.then((res)=> res.json())
-.then((data)=> setUsers(data))
-.catch((error)=> console.log(error))
-.finally(()=> setLoading(false))
-},[id])
-
-
-  return (
-    <>
-{
-loading ? <div>Loading...</div> :
-
-
-<div key={users.id}>
-<h1>User Details</h1>
-<p>Name: {users.name}</p>
-<p>Username: {users.username}</p>
-<p>Email: {users.email}</p>
-<p>Phone: {users.phone}</p>
-<p>Website: {users.website}</p>
-</div>
-
-}
-</>
-  )
+    return (
+        <div>
+            <h1>User Details</h1>
+            <p>Name: {user.name}</p>
+            <p>Username: {user.username}</p>
+            <p>Email: {user.email}</p>
+            <p>Phone: {user.phone}</p>
+            <p>Website: {user.website}</p>
+        </div>
+    );
 }
 
-export default UserDetails
+export default UserDetails;
